18c7b5c64cec2e18aeb53d9f7e1edcc7cff43e95
Hemjeeva <hemant.kumbhar@involve.ai>
2025-11-23 19:53:29 +0530

    First lesson

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,72 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { ITask } from './task.model';
+import { CreateTaskDto } from './create-task.dto';
+import { FindOneParams } from './find-one.params';
+import { UpdateTaskStatusDto } from './update-task-status.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  public findAll(): ITask[] {
+    return this.tasksService.findAll();
+  }
+
+  @Get('/:id')
+  public findOne(@Param() params: FindOneParams): ITask | undefined {
+    return this.findOneOrFail(params.id);
+  }
+
+  @Post()
+  public create(@Body() createTaskDto: CreateTaskDto) {
+    return this.tasksService.create(createTaskDto);
+  }
+
+//   @Patch('/:id/status')
+//   public updateStatus(
+//     @Param() params: FindOneParams,
+//     @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+//   ): ITask | undefined {
+//     const task = this.findOneOrFail(params.id);
+//     task.status = updateTaskStatusDto.status;
+//     return task;
+//   }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public deleteTask(@Param() params: FindOneParams): void {
+    const task = this.findOneOrFail(params.id);
+    this.tasksService.deleteTask(task);
+  }
+
+  @Patch('/:id')
+  public updateTask(@Param() params: FindOneParams, @Body() updateTaskDto: CreateTaskDto): ITask | undefined {
+      const task = this.findOneOrFail(params.id);
+
+      const updatedTask = this.tasksService.updateTask(task, updateTaskDto);
+      return updatedTask;
+  }
+
+  private findOneOrFail(id: string): ITask {
+    const task = this.tasksService.findOne(id);
+
+    if (!task) {
+      throw new NotFoundException();
+    }
+
+    return task;
+  }
+}
