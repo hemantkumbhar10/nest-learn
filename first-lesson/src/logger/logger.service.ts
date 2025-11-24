6d349d308aab2fdb7cc4c9beeb2bbbd5a16d483e
Hemant Kumbhar <hemantkumbhar010@gmail.com>
2025-11-24 16:59:03 +0530

    started with configs

--- first-lesson/src/logger/logger.service.ts
+++ first-lesson/src/logger/logger.service.ts
@@ -3,11 +3,11 @@ import { MessageFormatterService } from 'src/message-formatter/message-formatter
 
 @Injectable()
 export class LoggerService {
-    constructor(private readonly messageFormatter: MessageFormatterService) {}
+  constructor(private readonly messageFormatter: MessageFormatterService) {}
 
-    log(message: string): string {
-        const formattedMessage = this.messageFormatter.format(message);
-        console.log(formattedMessage);
-        return formattedMessage;
-    }
+  log(message: string): string {
+    const formattedMessage = this.messageFormatter.format(message);
+    console.log(formattedMessage);
+    return formattedMessage;
+  }
 }
